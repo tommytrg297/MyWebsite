8d1617dd0afed84f56b9a4395d980c2e9307ce10
Truong Tommy <truongtommy1999@gmail.com>
2022-03-03 02:53:14 -0500

    Small Update

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,7 @@ export default function About() {
             </p>
             <p className= "text-base text-md md:text-2xl text-center text-gray-700 leading-relaxed mt-4">
                 I am a Computer Science Major / Mathematics Minor at Rensselear Polytechnic Institute. 
-                I create applications for iOS devices as well as design User Interfaces in my spare time.
+                I work on for iOS applications as well as design User Interfaces in my spare time.
                 
             </p>
             <p className= "text-base text-md md:text-2xl text-center text-gray-700 leading-relaxed mt-4">
