e0717fed44b0142b2319fcb2b8b7f2acb7749dc0
Truong Tommy <truongtommy1999@gmail.com>
2022-03-05 19:10:13 -0500

    Update ShuttleTracker.js

--- src/components/ShuttleTracker.js
+++ src/components/ShuttleTracker.js
@@ -10,7 +10,6 @@ return (
     <div className="grid grid-cols-3 gap-7" > 
 
         <div className="col-span-2 bg-white shadow-lg rounded-xl p-2 "> 
-        
         <p className = " text-md sm:text-xl text-red-500 text-left font-semibold  pt-3 p-3 ">
             Never miss the shuttle again!</p>
 
@@ -19,8 +18,6 @@ return (
         When boarding a bus, tap "Board Bus" and choose a Shuttle. Tap "Leave Bus" when you get out. This will let users know that a Shuttle is at the stop. 
          </p>
 
-        
-
         <div className = " flex pt-8 pb-6">
        <img src={img1} className=" object-scale-down h-[32rem] w-2/5"></img>
        <img src={img2} className=" object-scale-down h-[32rem] w-3/5"></img>
@@ -28,9 +25,15 @@ return (
         </div>
 
 
+
+
         <div className=" col-span-1 bg-white shadow-lg rounded-xl p-2 "> 
             <p className = " text-md sm:text-xl text-red-500 text-left font-semibold  pt-3 p-3 ">
             Unique features</p>
+
+            <p className = " text-md sm:text-md text-black text-left font-semibold pt-0.5 pl-3 ">
+                Choose maximum boarding distance
+            </p>
         </div>
 
 
