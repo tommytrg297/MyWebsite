4c4772c8619cdb5bf49574107d820443963ed74e
Trương Như Phát 5 <truongtommy1999@gmail.com>
2022-08-15 10:27:55 +0700

    Changed About Me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -50,25 +50,26 @@ return (
                 SwiftUI and UIKit to build applications on Xcode.
             </p>
 
-            <div className="flex space-x-3 justify-center py-8">
+            <div className="flex space-x-3 justify-center py-4">
 
-            <img className = {programmingIcon} src={SwiftUI} alt="profile" ></img>
-            <p className= " text-6xl text-blue-500 py-2">  <strong> + </strong>   </p>
-            <img className = {programmingIcon} src={Xcode} alt="profile" ></img>
+            <img className = " w-24 bg-transparent drop-shadow-lg" src={SwiftUI} alt="profile" ></img>
+            <img className = " w-24 bg-transparent drop-shadow-lg" src={Swift} alt="profile" ></img>
+            <img className = " w-24 bg-transparent drop-shadow-lg" src={Xcode} alt="profile" ></img>
+
 
             </div>
 
-            <p className = " text-md sm:text-2xl text-black text-left font-semibold pt-0.5 pl-3 ">
+            <p className = " text-md sm:text-2xl text-black text-left font-semibold pl-3 ">
                 Frameworks include AVFoundations, MapKit, CoreML and multiple 3rd party libraries.
             </p>
 
-            <div className="flex space-x-5 justify-center py-8">
+            {/* <div className="flex space-x-5 justify-center py-8">
             <img className = {programmingIcon} src={AVFoundations} alt="profile" ></img>
             <img className = {programmingIcon} src={MapKit} alt="profile" ></img>
             <img className = {programmingIcon} src={CoreML} alt="profile" ></img>
-            </div>
+            </div> */}
 
-            <p className = " text-md sm:text-2xl text-black text-left font-semibold pt-0.5 pl-3 ">
+            {/* <p className = " text-md sm:text-2xl text-black text-left font-semibold pt-0.5 pl-3 ">
                 Firebase with REST API.
             </p>
 
@@ -81,8 +82,8 @@ return (
             </div>
             <p className= " text-5xl text-orange-400 py-4">  <strong> <FaArrowRight/> </strong>   </p>
             <img className = {programmingIcon} src={Firebase} alt="profile" ></img>
-
-            </div>
+            </div> */}
+            
         </div>
 
 
@@ -109,24 +110,24 @@ return (
             </div>
             </div>
             
-            <p className = " text-md sm:text-md text-black text-left font-semibold pt-0.5 pl-3 absolute bottom-24 ">
+            <p className = " text-md sm:text-md text-black text-left font-semibold pl-3 absolute bottom-16 ">
                 Connect with me: 
             </p>
 
-            <div className=" flex align-center justify-center mt-2 absolute bottom-12">
-            <a className="text-xl m-1 sm:m-2 sm:p-2 text-gray-800 hover:bg-gray-800 rounded-full 
+            <div className=" flex align-center justify-center mt-2 absolute bottom-6">
+            <a className="text-xl m-1 sm:m-2 sm:p-1 text-gray-800 hover:bg-gray-800 rounded-full 
                 hover:text-white transition-colors duration-300" href="https://github.com/TruongTommy99" target="_blank">
                 <FaGithub />
                     <span class  = "sr-only">Github</span>
             </a>
 
-            <a className="text-xl m-1 p-1 sm:m-2 sm:p-2  text-blue-600 hover:bg-blue-400 rounded-full 
+            <a className="text-xl m-1 p-1 sm:m-2 sm:p-1  text-blue-600 hover:bg-blue-400 rounded-full 
                 hover:text-white transition-colors duration-300" href="https://www.linkedin.com/in/tommy-tr/ " target="_blank">
                 <FaLinkedin />
                     <span class = "sr-only">LinkedIn</span>
             </a>
 
-            <a className="text-xl m-1 p-1 sm:m-2 sm:p-2  text-orange-500 hover:bg-orange-300 rounded-full 
+            <a className="text-xl m-1 p-1 sm:m-2 sm:p-1  text-orange-500 hover:bg-orange-300 rounded-full 
                 hover:text-white transition-colors duration-300" href="mailto: truongnhuphattommy@gmail.com?subject=Hello" target="_blank">
                 <FaEnvelope />
                     <span class = "sr-only">Mail</span>
