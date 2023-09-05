502e2b4b8e9cc11ba5777c3ec52edeab42fffcf5
Truong Tommy <truongtommy1999@gmail.com>
2023-09-05 00:08:13 -0400

    finished personal projects

--- src/components/personalProjects.js
+++ src/components/personalProjects.js
@@ -4,14 +4,20 @@ import img1 from "../images/python.svg.png";
 import {GoChevronRight} from "react-icons/go";
 import { SiShazam } from "react-icons/si";
 import {FaPaperPlane} from "react-icons/fa";
+
 import UniFriendIcon from "../images/UniFriend.png"
+import UniFriendsScreenshot from "../images/UniFriendsScreenshot.png"
+import ExplorerScreenshot from "../images/ExplorerScreenshot.png"
+import ExplorerScreenInformationView from "../images/ExplorerScreenshotInformationView.png"
+import ShazamScreenshot from "../images/ShazamScreenshot.png"
+
 
 
 
 function personalProjects() {
 
 return (
-    <div className="grid grid-cols-3 gap-7 " > 
+    <div className="grid grid-cols-3 gap-7 auto-cols-max " > 
 
         {/** Uni Friends*/}
         <div className="col-span-1 bg-white shadow-lg rounded-xl p-2">  
@@ -22,8 +28,9 @@ return (
                 </a>   {/* Title */}
             </div>
             <div className=" pl-5 pt-1 font-semibold">
-                <p className=""> An application that combines a music player with a timer allowing students to easily manage their studying block. </p>                     
+                <p className=""> Time your studying block with a relaxing music player.</p>                     
             </div>
+            <img src={UniFriendsScreenshot} className=" flex pb-4 rounded-xl  object-scale-down h-[32rem] px-16"></img>
         </div>
 
         {/** Explorer*/}
@@ -37,6 +44,8 @@ return (
             <div className=" pl-5 pt-1 font-semibold">
                 <p className=""> Find your next vacation spot and learn more about it.</p>                     
             </div>
+            <img src={ExplorerScreenshot} className="flex pb-4 rounded-xl object-scale-down h-[16rem] px-12 hover:scale-[3.5] transition duration-500 cursor-pointer"></img>
+            <img src={ExplorerScreenInformationView} className="flex pb-4 rounded-xl object-scale-down h-[16rem] px-12 hover:scale-[3.5] transition duration-500 cursor-pointer"></img>
         </div>
 
         {/** Shazam*/}
@@ -50,6 +59,7 @@ return (
             <div className=" pl-5 pt-1 font-semibold">
                 <p className=""> Start Shazaming with only a few lines of code. </p>                     
             </div>
+            <img src={ShazamScreenshot} className=" flex pb-4 rounded-xl  object-scale-down h-[32rem] px-16"></img>
         </div>
 
     </div>
